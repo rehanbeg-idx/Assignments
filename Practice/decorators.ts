435a78321b0da11ef2f3e6799d6f46bbe942d1f1
rehanbeg-idx <rehan.beg@indexnine.com>
2025-09-10 14:49:01 +0530

    Updated

---
+++ Practice/decorators.ts
@@ -0,0 +1,21 @@
+function logger<T extends new (...args: any[]) => any>(
+  target: T,
+  ctx: ClassDecoratorContext
+) {
+  console.log("This a decorator");
+  console.log(target);
+  console.log(ctx);
+
+  return class extends target {
+    age: number = 35;
+  };
+}
+
+@logger
+class App {
+  name: string = "abc";
+
+  greet() {
+    console.log("Hello " + name);
+  }
+}
