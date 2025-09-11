58157d84c2c11e09061935e084e5935907d79769
rehanbeg-idx <rehan.beg@indexnine.com>
2025-09-11 17:00:07 +0530

    Updated

---
+++ Drag & Drop/src/app.ts
@@ -0,0 +1,25 @@
+class Project {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prj = new Project();
