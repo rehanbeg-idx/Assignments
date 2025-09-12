fbc6ba6f4c020b560a7da02b8165c0b270a7b9a7
rehanbeg-idx <rehan.beg@indexnine.com>
2025-09-12 11:20:35 +0530

    Updated

--- Drag & Drop/src/app.ts
+++ Drag & Drop/src/app.ts
@@ -1,7 +1,25 @@
+// autobind decorator
+
+function autobind(_ : any, _2 : string, descriptor : PropertyDescriptor){
+  const originalMethod = descriptor.value;
+  const adjDescriptor : PropertyDescriptor ={
+    configurable : true,
+    get(){
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescriptor;
+}
+
+// Project Input Class
 class Project {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +32,24 @@ class Project {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  @autobind
+  private submitHandler(event : Event){
+    event.preventDefault();
+    console.log(this.titleInputElement.value)
+  }
+
+  private configure(){
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
@@ -22,4 +57,3 @@ class Project {
   }
 }
 
-const prj = new Project();
