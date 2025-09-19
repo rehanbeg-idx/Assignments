c2763e7d43b1573bc9bd09327e3bce3b1c5cac85
rehanbeg-idx <rehan.beg@indexnine.com>
2025-09-19 10:42:50 +0530

    Updated

--- Practice/https/http.js
+++ Practice/https/http.js
@@ -1,6 +1,21 @@
-var http  = require('http');
+var http = require("http");
+var fs = require("fs");
 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
+// http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+//     var message = 'Hey how are you?';
+//     html = html.replace('{message}', message);
+//     res.end(html);
+// }).listen(3000, '127.0.0.1');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, "content-Type", { "Content-Type": "application/json" });
+    var obj = {
+      firstName: "abc",
+      lastName: "xyz",
+    };
+
+    res.end(JSON.stringify(obj));
+  })
+  .listen(3000, "127.0.0.1");
