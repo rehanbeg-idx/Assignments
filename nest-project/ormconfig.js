93e4595f6ee6ae86812b11043af1ce14573d7e7e
rehanbeg-idx <rehan.beg@indexnine.com>
2025-09-29 10:06:19 +0530

    Updated

--- nest-project/ormconfig.js
+++ nest-project/ormconfig.js
@@ -1,8 +1,8 @@
 const { DataSource } = require('typeorm');
 
 const dbConfig = {
-  type: 'sqlite',           // default type
-  database: 'db.sqlite',    // default database
+  type: 'sqlite',
+  database: 'db.sqlite',
   synchronize: false,
   entities: ['**/*.entity.js'],
   migrations: ['migrations/*.js'],
@@ -11,31 +11,31 @@ const dbConfig = {
   },
 };
 
-// Adjust config based on environment
 switch (process.env.NODE_ENV) {
   case 'development':
     Object.assign(dbConfig, {
+      type: 'sqlite',
       database: 'db.sqlite',
       entities: ['**/*.entity.js'],
     });
     break;
   case 'test':
     Object.assign(dbConfig, {
+      type: 'sqlite',
       database: 'test.sqlite',
       entities: ['**/*.entity.ts'],
+      migrationsRun: true,
     });
     break;
   case 'production':
-    // add production settings if needed
     break;
   default:
     throw new Error('Unknown Environment');
 }
 
-// Create DataSource for TypeORM CLI
 const AppDataSource = new DataSource(dbConfig);
 
 module.exports = {
-  dbConfig,      // for NestJS runtime
-  AppDataSource, // for CLI commands
+  dbConfig,
+  AppDataSource,
 };
--- nest-project/src/app.module.ts
+++ nest-project/src/app.module.ts
@@ -7,15 +7,12 @@ import { ReportsModule } from './reports/reports.module';
 import cookieSession from 'cookie-session';
 import * as path from 'path';
 
-// Resolve absolute path to root ormconfig.js
-const {dbConfig} = require(path.resolve(__dirname, '..', '..', 'ormconfig.js'));
+const { dbConfig } = require(
+  path.resolve(__dirname, '..', '..', 'ormconfig.js'),
+);
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(dbConfig),
-    UsersModule,
-    ReportsModule,
-  ],
+  imports: [TypeOrmModule.forRoot(dbConfig), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
